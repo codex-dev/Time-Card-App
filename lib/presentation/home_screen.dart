c29a9125df1114a0dba506694126aeafefac5105
Ravindu Fernando <ravindu.fernando@itelasoft.com>
2024-01-30 13:02:48 +0530

    navigate to previous & next day, display listview

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -12,29 +12,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime _selectedDate = DateTime.now();
+  late DateTime _selectedDate;
   late String dateYMD;
+  late String dateDisplay;
   late final ShiftsDB shiftsDB;
-  late List<Shift> listAllShifts;
-
-  // late DailyRecord dailyRecord = DailyRecord(dateYMD, 0, 0.0, []);
+  List<Shift> listAllShifts = [];
 
   @override
   void initState() {
     super.initState();
     shiftsDB = ShiftsDB();
-    dateYMD = DateFormat("yyyy/MM/dd").format(_selectedDate);
+    changeSelectedDate(DateTime.now());
 
-    testDbOperations();
+    // testDbOperations();
   }
 
   Future<void> testDbOperations() async {
     shiftsDB.deleteShift(1);
-    
+
     listAllShifts = await shiftsDB.getAllShiftsByDate(workDate: dateYMD);
     debugPrint(
         '// Found ${listAllShifts.length} shift${listAllShifts.length > 1 ? 's' : ''} in $dateYMD');
-
 
     // shiftsDB.addShift(
     //     workDate: dateYMD,
@@ -62,7 +60,7 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  Future<void> _selectDate(BuildContext context) async {
+  Future<void> selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
         context: context,
         initialDate: _selectedDate,
@@ -70,9 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
         lastDate: DateTime(2101));
 
     if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-      });
+      changeSelectedDate(picked);
     }
   }
 
@@ -85,21 +81,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return SizedBox(
-                  height: 200,
-                  child: Center(
-                    child: ElevatedButton(
-                      child: const Text('Close'),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-                );
-              });
+          showShiftDetailsForm(context);
         },
         child: const Icon(Icons.add),
       ),
@@ -109,11 +91,130 @@ class _HomeScreenState extends State<HomeScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              GestureDetector(
-                  onTap: () => _selectDate(context), child: Text(dateYMD))
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_rounded),
+                    onPressed: () {
+                      changeSelectedDate(
+                          _selectedDate.subtract(const Duration(days: 1)));
+                    },
+                  ),
+                  GestureDetector(
+                      onTap: () => selectDate(context),
+                      child: Text(
+                        dateDisplay,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      )),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward_ios_rounded),
+                    onPressed: () {
+                      changeSelectedDate(
+                          _selectedDate.add(const Duration(days: 1)));
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              if (listAllShifts.isNotEmpty) ...[
+                ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      Shift shiftItem = listAllShifts[index];
+
+                      return displayShiftItem(shiftItem, context);
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(
+                          height: 10,
+                        ),
+                    itemCount: listAllShifts.length)
+              ] else ...[
+                const Text(
+                  'No shifts found for the day.',
+                  style: TextStyle(
+                      fontSize: 16, color: Color.fromARGB(255, 121, 121, 121)),
+                )
+              ]
             ],
           )),
     );
+  }
+
+  Container displayShiftItem(Shift shiftItem, BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 221, 221, 221),
+          borderRadius: BorderRadius.circular(10)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                shiftItem.employeeName,
+                textAlign: TextAlign.start,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              Text(
+                shiftItem.employeeEmail,
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                    fontWeight: FontWeight.normal, fontSize: 16),
+              )
+            ],
+          ),
+          Text(
+            '${shiftItem.checkInTime} - ${(shiftItem.checkOutTime == null || shiftItem.checkOutTime!.isEmpty) ? 'now' : shiftItem.checkOutTime}',
+            textAlign: TextAlign.start,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          IconButton(
+            icon: const Icon(Icons.edit_note_outlined),
+            onPressed: () => showShiftDetailsForm(context),
+          )
+        ],
+      ),
+    );
+  }
+
+  void changeSelectedDate(DateTime newDate) async {
+    _selectedDate = newDate;
+    dateYMD = DateFormat('yyyy/MM/dd').format(_selectedDate);
+    dateDisplay = DateFormat('E, dd MMMM yyyy').format(_selectedDate);
+
+    listAllShifts = await shiftsDB.getAllShiftsByDate(workDate: dateYMD);
+
+    setState(() {});
+  }
+
+  Future<void> showShiftDetailsForm(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return SizedBox(
+            height: 200,
+            child: Center(
+              child: ElevatedButton(
+                child: const Text('Close'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          );
+        });
   }
 
   addPrefix(int value) => value < 10 ? "0$value" : value;
